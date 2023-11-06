d3eb6b67c7052c762db9e0b5c3da07d97823edc4
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-06 19:47:57 +0100

    added security packages

--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Repository/StudentRepository.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Repository/StudentRepository.java
@@ -1,11 +1,18 @@
 package com.Group11Project.ClassAttendanceSystem.Repository;
 
+import com.Group11Project.ClassAttendanceSystem.Model.Role;
 import com.Group11Project.ClassAttendanceSystem.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByUsername(String username);
+
+
 
 }
