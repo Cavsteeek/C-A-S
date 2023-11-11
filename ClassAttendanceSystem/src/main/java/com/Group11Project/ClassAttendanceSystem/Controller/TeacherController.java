70d4aa9fb238d9668dfb7ac17e0a1b6b86c08c66
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-11 05:50:19 -1100

    implemented error logging for all methods in the teacher controller

--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/TeacherController.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/TeacherController.java
@@ -36,76 +36,122 @@ public class TeacherController {
 
     }
 
-    @DeleteMapping("/delete-student/{id}") //delete student by id
-    public ResponseEntity<Void> deleteStudentById(@PathVariable("id") Long id){
-        teacherService.deleteStudentById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @DeleteMapping("/delete-student/{id}") // delete student by id
+    public ResponseEntity<Void> deleteStudentById(@PathVariable("id") Long id) {
+        try {
+            teacherService.deleteStudentById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("/get-all-students") //Get all students
-    public ResponseEntity<List<Student>> findAllStudents(){
-        List<Student> students = teacherService.findAllStudents();
-        if (!students.isEmpty()) {
-            return new ResponseEntity<>(students, HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/get-all-students") // Get all students
+    public ResponseEntity<List<Student>> findAllStudents() {
+        try {
+            List<Student> students = teacherService.findAllStudents();
+            if (!students.isEmpty()) {
+                return new ResponseEntity<>(students, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
 
     @GetMapping("/get-student-by-fname/{fName}") //Get student by first name
     public ResponseEntity<List<Student>> findByfName(@PathVariable("fName")String fName){
-        List<Student> students = teacherService.findStudentByfName(fName);
-        if (!students.isEmpty()){
-            return new ResponseEntity<>(students, HttpStatus.OK);
+        try{
+            List<Student> students = teacherService.findStudentByfName(fName);
+            if (!students.isEmpty()){
+                return new ResponseEntity<>(students, HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+
     }
 
     @GetMapping("/get-student-by-lname/{lName}") //Get student by last name
     public ResponseEntity<List<Student>> findBylName(@PathVariable("lName")String lName){
-        List<Student> students = teacherService.findStudentBylName(lName);
-        if (!students.isEmpty()){
-            return new ResponseEntity<>(students, HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            List<Student> students = teacherService.findStudentBylName(lName);
+            if (!students.isEmpty()){
+                return new ResponseEntity<>(students, HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/delete-all-students") //Delete all students
     public ResponseEntity<Void> deleteAllStudents(){
-        teacherService.deleteStudents();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            teacherService.deleteStudents();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/add-course") // create new course
     public ResponseEntity<Course> newCourse (@RequestBody Course course)
     {
-        Course course1 = teacherService.saveCourse(course);
-        return new ResponseEntity<>(course1, HttpStatus.CREATED);
+        try {
+            Course course1 = teacherService.saveCourse(course);
+            return new ResponseEntity<>(course1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @PutMapping("/update-course/{id}") //Update course details by id
     public ResponseEntity<Course> updateCourseById(@PathVariable("id") Long id, @RequestBody Course course)
     {
-        course.setId(id);
-        Course updatedCourse = teacherService.updateCourseById(id, course).getBody();
+       try {
+           course.setId(id);
+           Course updatedCourse = teacherService.updateCourseById(id, course).getBody();
 
-        if (updatedCourse != null) {
-            return new ResponseEntity<>(updatedCourse,HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-        }
+           if (updatedCourse != null) {
+               return new ResponseEntity<>(updatedCourse,HttpStatus.OK);
+           } else {
+               return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+           }
+       } catch (Exception e) {
+           // Log the exception
+           e.printStackTrace();
+           return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+       }
     }
 
     @DeleteMapping("/delete-course/{id}") // delete course by id
     public ResponseEntity<Void> deleteCourseById(@PathVariable Long id)
     {
-        teacherService.deleteCourseById(id);
-        return new  ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            teacherService.deleteCourseById(id);
+            return new  ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/get-course-by-cname/{cName}") // get course by name
@@ -136,12 +182,17 @@ public class TeacherController {
 
     @GetMapping("/get-all-courses") // get all courses
     public ResponseEntity<List<Course>> findAllCourse(){
-        List<Course> courses = teacherService.findAllCourses();
-        if (!courses.isEmpty()) {
-            return new ResponseEntity<>(courses, HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            List<Course> courses = teacherService.findAllCourses();
+            if (!courses.isEmpty()) {
+                return new ResponseEntity<>(courses, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -164,24 +215,38 @@ public class TeacherController {
 
     @DeleteMapping("/delete-all-courses") // delete all courses
     public ResponseEntity<Void> deleteCourse(){
-        teacherService.deleteCourses();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            teacherService.deleteCourses();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/delete-attendance/{id}") // delete attendance by id
     public ResponseEntity<Void> deleteRecordById(@PathVariable Long id)
-    {
-        teacherService.deleteARById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    {   try {
+            teacherService.deleteARById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+        // Log the exception
+        e.printStackTrace();
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     }
 
     @DeleteMapping("/delete-all-attendance") // delete all attendance
     public ResponseEntity<Void> deleteRecord(){
-        teacherService.deleteRecord();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            teacherService.deleteRecord();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
-
-
 
 }
