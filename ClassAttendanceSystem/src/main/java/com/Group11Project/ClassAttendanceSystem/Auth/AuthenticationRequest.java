1f9f1a2c5b8e2b2b2c5dd5615c84a20eedd50011
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-20 02:00:04 +0100

    implementing jwt and security

---
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.Group11Project.ClassAttendanceSystem.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    String matricNumber;
+    String password;
+}
--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/AuthenticationController.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/AuthenticationController.java
@@ -1,6 +1,11 @@
 package com.Group11Project.ClassAttendanceSystem.Controller;
 
+import com.Group11Project.ClassAttendanceSystem.Auth.AuthenticationRequest;
+import com.Group11Project.ClassAttendanceSystem.Auth.AuthenticationResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/cas/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    @PostMapping("/student/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody AuthenticationRequest request
+    ){
+
+    }
+
+    @PostMapping("/teacher/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody AuthenticationRequest request
+    ){
+
+    }
 }
