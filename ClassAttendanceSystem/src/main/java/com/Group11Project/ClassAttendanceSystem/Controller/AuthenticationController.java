5d6a817a22c0ee30d2479bc1019bc70a978e3925
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-23 17:50:08 +0100

    implementing jwt and security

--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/AuthenticationController.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/AuthenticationController.java
@@ -1,7 +1,12 @@
 package com.Group11Project.ClassAttendanceSystem.Controller;
 
 import com.Group11Project.ClassAttendanceSystem.Service.AuthenticationService;
+import com.Group11Project.ClassAttendanceSystem.dto.JwtAuthenticationResponse;
+import com.Group11Project.ClassAttendanceSystem.dto.SigninRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest signinRequest){
+        return ResponseEntity.ok(authenticationService.signin(signinRequest));
+    }
 }
--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Service/AuthenticationService.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Service/AuthenticationService.java
@@ -1,4 +1,10 @@
 package com.Group11Project.ClassAttendanceSystem.Service;
 
+import com.Group11Project.ClassAttendanceSystem.dto.JwtAuthenticationResponse;
+import com.Group11Project.ClassAttendanceSystem.dto.RefreshTokenRequest;
+import com.Group11Project.ClassAttendanceSystem.dto.SigninRequest;
+
 public interface AuthenticationService {
+    JwtAuthenticationResponse signin(SigninRequest signinRequest);
+    JwtAuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
 }
