9f01461bc4fcc8278d31d06a85ae17dedeb402d7
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-28 17:12:59 +0100

    implementing front-end

--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/TeacherController.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Controller/TeacherController.java
@@ -1,8 +1,13 @@
 package com.Group11Project.ClassAttendanceSystem.Controller;
 
+import com.Group11Project.ClassAttendanceSystem.Model.AttendanceRecord;
+import com.Group11Project.ClassAttendanceSystem.Model.Course;
 import com.Group11Project.ClassAttendanceSystem.Model.Role;
 import com.Group11Project.ClassAttendanceSystem.Model.User;
+import com.Group11Project.ClassAttendanceSystem.Repository.AttendanceRepository;
+import com.Group11Project.ClassAttendanceSystem.Repository.CourseRepository;
 import com.Group11Project.ClassAttendanceSystem.Repository.UserRepository;
+import com.Group11Project.ClassAttendanceSystem.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +25,8 @@ import java.util.List;
 public class TeacherController
 {
     private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
+    private final AttendanceRepository attendanceRepository;
     @GetMapping
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Welcome Teacher");
@@ -30,6 +37,17 @@ public class TeacherController
         List<User> studentList = userRepository.findAllByRole(Role.STUDENT);
         return ResponseEntity.ok(studentList);
 
+    }
+    @GetMapping("/get-all-courses")
+    public ResponseEntity<List<Course>> findAllCourse(){
+        List<Course> courseList = courseRepository.findAll();
+        return ResponseEntity.ok(courseList);
+    }
+
+    @GetMapping("view-all-attendance")
+    public ResponseEntity<List<AttendanceRecord>> viewAllAttendance() {
+        List<AttendanceRecord> attendanceRecords = attendanceRepository.findAll();
+        return ResponseEntity.ok(attendanceRecords);
     }
 
 }
