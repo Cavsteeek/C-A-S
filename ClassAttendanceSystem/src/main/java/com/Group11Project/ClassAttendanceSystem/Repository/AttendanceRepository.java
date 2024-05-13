47b260fe5f7b0fef9bf980a377bd94bfd1b35c66
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-05-13 09:52:45 +0100

    commit

--- ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Repository/AttendanceRepository.java
+++ ClassAttendanceSystem/src/main/java/com/Group11Project/ClassAttendanceSystem/Repository/AttendanceRepository.java
@@ -2,6 +2,8 @@ package com.Group11Project.ClassAttendanceSystem.Repository;
 
 import com.Group11Project.ClassAttendanceSystem.Model.AttendanceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AttendanceRepository extends JpaRepository<AttendanceRecord, Long> {
 }
